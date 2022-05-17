16a8603a4997d69bb0e6d924296ee97c52c439b3
Buzz88-Dev <omarbuzzoni88@gmail.com>
2022-05-17 20:50:16 +0200

    ended

--- js/script.js
+++ js/script.js
@@ -5,55 +5,84 @@
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare
 // sono stati individuati.
 
-// stampare 5 numeri casuali sul browser
 let divElements = document.getElementById("number");
-let numbers = [];
-
-for (let i = 0; i < 5; i ++){
-    let number = Math.floor(Math.random() * 100) + 1;
-    numbers.push(number);
-    divElements.innerHTML += " " + number + " ";
-}
-console.log(numbers);
-
-
-// creo una funzione time di 10 secondi
-let divTime = document.getElementById("time");
-let time = 0;
-
-let startTime = setInterval(function (){
-        if (time === 10){
-            clearInterval(time);
-            divElements.innerHTML = " ";
-            divTime.innerHTML = "Tempo scaduto"
-
-        } else {
-            time++;
-            divTime.innerHTML = time;
-        }
-    }
-, 1000)
-
+let click = document.getElementById("myButton");
 let resulte = document.getElementById("risultato");
+let divTime = document.getElementById("time");
+divTime.classList.add("center");
+let numbers = [];
+let time = 10;
 let count = 0;
 
-// creo la funzione che si attiva dopo 12 secondi dall'apertura del browser
-let endtime = setInterval(
-    function(){
-        let numbUser;
-        let i = 0
-        while (i < 5){
-            numbUser = parseInt(prompt("Dammi un numero"));
-            console.log(numbUser);
-        
-            if(numbers.includes(numbUser)){
-                count++;
-            }
+click.addEventListener("click",
 
-            i++;
+    function(){
+        for (let i = 0; i < 5; i ++){
+            let number = Math.floor(Math.random() * 100) + 1;
+            numbers.push(number);
+            divElements.innerHTML += " " + number + " ";
         }
-        clearInterval(endtime);
-        console.log("Hai indovinato: " + count + " numeri");
-        resulte.innerHTML = "Hai indovinato: " + count + " numeri";
+        console.log(numbers);
+
+        setInterval(function (){
+            if (time === 0){
+                clearInterval(time);
+                divElements.innerHTML = "Tempo scaduto";          
+                divTime.innerHTML = " ";
+    
+            } else {
+                time--;
+                divTime.innerHTML = "Hai " + time + " secondi di tempo per memorizzarli";
+            }
+        }
+    , 1000)
+
+
+    let endtime = setInterval(
+        function(){
+            let numbUser;
+            let i = 0
+            alert("Bene, ora ti ricordi che numeri erano???....Clicca ok");
+            while (i < 5){
+                numbUser = parseInt(prompt("Dammi un numero"));
+                console.log(numbUser);
+            
+                if(numbers.includes(numbUser)){
+                    count++;
+               }
+    
+               i++;
+            }
+            clearInterval(endtime);
+            console.log("Hai indovinato: " + count + " numeri");
+            switch (count){
+    
+                case 5:
+                resulte.innerHTML = "Bravo!!Ottima memoria. Hai indovinato tutti e " + count + " i numeri";
+                break;
+    
+                case 4:
+                resulte.innerHTML = "Bravo!! Hai indovinato " + count + " numeri";
+                break;
+    
+                case 3:
+                resulte.innerHTML = "Bene ma non benissimo!! Hai indovinato " + count + " numeri";
+                break;
+    
+                case 2:
+                resulte.innerHTML = "Mmmm potevi fare meglio. Hai indovinato " + count + " numeri";
+                break;
+    
+                case 1:
+                resulte.innerHTML = "Male Male, hai indovinato solo " + count + " numeri";
+                break;
+    
+                case 0:
+                resulte.innerHTML = "Pessimo!!Non ci siamo proprio, non hai indovinato manco un numero";
+                break;
+            }
+        }
+    , 12000)
     }
-, 12000)
+)
+
